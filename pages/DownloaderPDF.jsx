646abd463333ad9e0802b5c14244a28ff167d384
Your Name <you@example.com>
2023-05-12 17:05:28 +0200

    DC

--- pages/DownloaderPDF.jsx
+++ pages/DownloaderPDF.jsx
@@ -1,12 +1,6 @@
-import { useState, useEffect } from 'react'
-import { generateUUID } from '../utils/UIDgenerator'
-import { writeUserData } from '../firebase/utils'
-import Button from '../components/Button'
+
 import styles from '../styles/Uuid.module.css'
-import Image from 'next/image'
 import Layout from '../layout/Layout'
-import { useUser } from '../context/Context.js'
-import { WithAuth } from '../HOCs/WithAuth'
 
 import { useRouter } from 'next/router'
 
@@ -17,43 +11,10 @@ const InvoicePDF = dynamic(() => import("../components/pdf"), {
 });
 
 function UuidController() {
-  const { user, userDB, setUserSuccess, success, uuid, setUuid } = useUser()
   const router = useRouter()
 
 
-  function generate() {
-    let uuidGenerates = []
-    for (let i = 0; i < 16; i++) {
-      const newUuid = generateUUID()
-      uuidGenerates.push(newUuid)
-    }
-    setUuid([...uuidGenerates])
-  }
-
-  function añadir() {
-    console.log('anadiendo codes')
-    const obj = uuid.reduce(function (target, key, index) {
-      target[key] = false
-      return target;
-    }, {})
-    return writeUserData('/activadores', obj, setUserSuccess)
-  }
-
-
-  function redirect() {
-    const obj = uuid.reduce(function (target, key, index) {
-      target[key] = false
-      return target;
-    }, {})
-    router.push('/PDFdoc')
-    return writeUserData('/activadores', obj, setUserSuccess)
-  }
-  useEffect(() => {
-
-  }, []);
-
-  const obj = new URLSearchParams(router.query)
-  console.log(JSON.parse(router.query.image))
+//   console.log(JSON.parse(router.query.image))
 
 
   return (
@@ -66,7 +27,7 @@ function UuidController() {
                 <br />
                 <br />
 
-            <InvoicePDF image={JSON.parse(router.query.image)} dataUrl={router.query.dataUrl}  />
+            {/* <InvoicePDF image={JSON.parse(router.query.image)} dataUrl={router.query.dataUrl}  /> */}
             </div>      
        </div>
     </Layout>
