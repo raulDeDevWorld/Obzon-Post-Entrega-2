dc0fa913365322adb65c9d7bd31a8096daf1fc8a
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2023-05-13 20:41:41 -0400

    Update DownloaderPDF.jsx
    Cc

--- pages/DownloaderPDF.jsx
+++ pages/DownloaderPDF.jsx
@@ -36,7 +36,7 @@ function UuidController() {
           <br />
           <br />
 
-          {<InvoicePDF img={userImage} dbUrl={router.query.dataUrl && router.query.dataUrl.replaceAll(' ', '+')} />}
+          {userImage && <InvoicePDF img={userImage} dbUrl={router.query.dataUrl && router.query.dataUrl.replaceAll(' ', '+')} />}
         </div>
       </div>
     </Layout>
